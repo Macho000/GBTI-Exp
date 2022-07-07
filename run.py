a648fbb879a3334eecc7dd2871e7866e41f072fe
Macho000 <saburotyan758@gmail.com>
2022-07-07 22:35:14 +0900

    20220707 mock preprocess

---
+++ run.py
@@ -0,0 +1,168 @@
+import hydra
+from dgl.dataloading import NodeDataLoader, MultiLayerFullNeighborSampler, MultiLayerNeighborSampler
+from omegaconf import DictConfig, OmegaConf
+from utils import *
+import torch
+import JointGT
+
+
+@hydra.main(config_name='config.yaml')
+def main(cfg : DictConfig) -> None:
+    set_logger(cfg)
+    use_cuda = cfg.model.cuda and torch.cuda.is_available()
+    data_path = os.path.join(cfg.dataset.data_dir, cfg.dataset.name, 'clean')
+    save_path = os.path.join(cfg.model.save_dir, cfg.dataset.name)
+
+    # graph
+    e2id = read_id(os.path.join(data_path, 'entities.tsv'))
+    id2e = read_entity(os.path.join(data_path, 'entities.tsv'))
+    r2id = read_id(os.path.join(data_path, 'relations.tsv'))
+    id2r = read_entity(os.path.join(data_path, 'relations.tsv'))
+    length_r2id = len(r2id)
+    r2id['type'] = length_r2id
+    id2r[length_r2id] = 'type'
+    t2id = read_id(os.path.join(data_path, 'types.tsv'))
+    id2t = read_entity(os.path.join(data_path, 'types.tsv'))
+    if cfg.dataset.name=="FB15kET": mid2name = read_name(os.path.join(data_path, 'mid2name.tsv'))
+    num_entity = len(e2id)
+    num_rels = len(r2id)
+    num_types = len(t2id)
+    num_nodes = num_entity + num_types
+    g, train_label, all_true, train_id, valid_id, test_id = load_graph(data_path, e2id, r2id, t2id,
+                                                                       cfg.preprocess.load_ET, cfg.preprocess.load_KG)
+
+    if cfg.preprocess.neighbor_sampling:
+        train_sampler = MultiLayerNeighborSampler([cfg.preprocess.neighbor_num] * cfg.preprocess.num_layers)
+
+    else:
+        train_sampler = MultiLayerFullNeighborSampler(cfg.preprocess.num_layers)
+    test_sampler = MultiLayerFullNeighborSampler(cfg.preprocess.num_layers)
+    train_dataloader = NodeDataLoader(
+        g, train_id, train_sampler,
+        batch_size=cfg.model.train_batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=1
+    )
+    valid_dataloader = NodeDataLoader(
+        g, valid_id, test_sampler,
+        batch_size=cfg.model.test_batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=6
+    )
+
+    # preprocess train and valid for language model's input
+    if not os.path.exists(os.path.join(data_path, 'train_input.txt')):
+        # make tuple (subject, relation, object)
+        text_input = []
+        for input_nodes, output_nodes, blocks in train_dataloader:
+            # get name from mid: mid2name[id2e[?]]
+            # get name rels: id2r[?%num_rels]
+            text_tuple = ""
+            for index, entity_id in enumerate(blocks[0].srcdata['_ID']):
+                if index==0:
+                    continue
+                # change id to name
+                # check type or entity
+                if entity_id.item()>=len(e2id):
+                    # if entity_id is type
+                     # check incoming or outcoming graph
+                    if blocks[0].edata["etype"][index-1].item()>=num_rels:
+                        # if outgoing graph
+                        obj = mid2name[id2e[blocks[0].dstdata['_ID'].item()]]
+                        rel = id2r[blocks[0].edata["etype"][index-1].item()%num_rels]
+                        sub = id2t[entity_id.item()%len(e2id)]
+                    else:
+                        # if incoming graph
+                        sub = mid2name[id2e[blocks[0].dstdata['_ID'].item()]]
+                        rel = id2r[blocks[0].edata["etype"][index-1].item()]
+                        obj = id2t[entity_id.item()%len(e2id)]
+                else:
+                    # if entity_id is entity
+                    # check incoming or outcoming graph
+                    if blocks[0].edata["etype"][index-1].item()>=num_rels:
+                        # if outcoming graph
+                        sub = mid2name[id2e[blocks[0].dstdata['_ID'].item()]]
+                        rel = id2r[blocks[0].edata["etype"][index-1].item()%num_rels]
+                        obj = mid2name[id2e[entity_id.item()]]
+                    else:
+                        # if incoming graph
+                        sub = mid2name[id2e[blocks[0].dstdata['_ID'].item()]]
+                        rel = id2r[blocks[0].edata["etype"][index-1].item()]
+                        obj = mid2name[id2e[entity_id.item()]]
+                text_tuple += " <H> "+ sub + " <R> " + rel + " <T> " + obj
+            text_input.append(text_tuple.strip())
+            with open(os.path.join(data_path, 'train_input.txt'), 'w') as f:
+                for item in text_input:
+                    f.write("%s\n" % item)
+
+            
+            break
+    # model
+    if cfg.model.name == 'JointGT':
+        model = JointGT(cfg, num_nodes, num_rels, num_types)
+    elif cfg.model.name == 'T5':
+        model = T5()
+    else:
+        raise ValueError('No such model')
+
+    if use_cuda:
+        model = model.to('cuda')
+    for name, param in model.named_parameters():
+        logging.debug('Parameter %s: %s, require_grad=%s' % (name, str(param.size()), str(param.requires_grad)))
+
+    # optimizer
+    optimizer = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=cfg.optimizer.learning_rate,
+    )
+
+    # loss
+    criterion = torch.nn.BCELoss()
+
+    # training
+    # max_valid_mrr = 0
+    # model.train()
+    # for epoch in range(cfg.model.max_epoch):
+    #     log = []
+    #     for input_nodes, output_nodes, blocks in train_dataloader:
+    #         label = train_label[output_nodes, :]
+    #         if use_cuda:
+    #             blocks = [b.to(torch.device('cuda')) for b in blocks]
+    #             label = label.cuda()
+    #         loss = model(blocks)
+
+    #         log.append({
+    #             "loss": loss.item(),
+    #         })
+
+    #         optimizer.zero_grad()
+    #         loss.backward()
+    #         optimizer.step()
+
+    #     avg_loss = sum([_['loss'] for _ in log]) / len(log)
+    #     logging.debug('epoch %d: loss: %f' %
+    #                   (epoch, avg_loss))
+
+        # if epoch != 0 and epoch % cfg.model.valid.max_epoch == 0:
+        #     torch.save(model.state_dict(), os.path.join(save_path, 'model.pkl'))
+        #     model.eval()
+        #     with torch.no_grad():
+        #         predict = torch.zeros(num_entity, num_types, dtype=torch.half)
+        #         for input_nodes, output_nodes, blocks in valid_dataloader:
+        #             if use_cuda:
+        #                 blocks = [b.to(torch.device('cuda')) for b in blocks]
+        #             # predict[output_nodes] = model(blocks).cpu().half()
+        #         valid_mrr = evaluate(os.path.join(data_path, 'ET_valid.txt'), predict, all_true, e2id, t2id)
+        #     model.train()
+        #     if valid_mrr < max_valid_mrr:
+        #         logging.debug('early stop')
+        #         break
+        #     else:
+        #         torch.save(model.state_dict(), os.path.join(save_path, 'best_model.pkl'))
+        #         max_valid_mrr = valid_mrr
+
+
+if __name__=='__main__':
+  main()
---
+++ utils.py
@@ -0,0 +1,179 @@
+import logging
+import os
+import torch
+import dgl
+
+def set_logger(cfg):
+    """
+    setting logging
+    e.g. logging path, basic Config
+
+    Parameter
+    ----------
+    cfg
+
+    """
+    if not os.path.exists(os.path.join(cfg.model.save_dir,cfg.dataset.name)):
+        os.makedirs(os.path.join(os.getcwd(),cfg.model.save_dir, cfg.dataset.name))
+
+    log_file = os.path.join(cfg.model.save_dir, cfg.dataset.name, 'log.txt')
+
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename=log_file,
+        filemode='w'
+    )
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+def read_id(path):
+    """
+    loading graph from dataset
+    
+    Parameters
+    ----------
+    path: string
+
+    Returns
+    -------
+    tmp: dict (key=entity, value=type)
+    """
+    tmp = dict()
+    with open(path, encoding='utf-8') as r:
+        for line in r:
+            e, t = line.strip().split('\t')
+            tmp[e] = int(t)
+    return tmp
+
+def read_entity(path):
+    """
+    loading graph from dataset
+    
+    Parameters
+    ----------
+    path: string
+
+    Returns
+    -------
+    tmp: dict (key=id, value=(type,entity,relation))
+    """
+    tmp = dict()
+    with open(path, encoding='utf-8') as r:
+        for line in r:
+            e, t = line.strip().split('\t')
+            tmp[int(t)] = e
+    return tmp
+
+def read_name(path):
+    tmp = dict()
+    with open(path, encoding='utf-8') as r:
+        for line in r:
+            mid, name = (line.strip().split('\t') + [None])[:2]
+            tmp[mid] = name
+    return tmp
+
+def load_labels(paths, e2id, t2id):
+    labels = torch.zeros(len(e2id), len(t2id))
+    for path in paths:
+        with open(path, encoding='utf-8') as r:
+            for line in r:
+                e, t = line.strip().split('\t')
+                e_id, t_id = e2id[e], t2id[t]
+                labels[e_id, t_id] = 1
+    return labels
+
+
+def load_id(path, e2id):
+    ret = set()
+    with open(path, encoding='utf-8') as r:
+        for line in r:
+            e, t = line.strip().split('\t')
+            ret.add(e2id[e])
+    return list(ret)
+
+def load_triple(path, e2id, r2id):
+    head = []
+    e_type = []
+    tail = []
+    with open(path, encoding='utf-8') as r:
+        for line in r:
+            h, r, t = line.strip().split('\t')
+            h, r, t = e2id[h], r2id[r], e2id[t]
+            head.append(h)
+            e_type.append(r)
+            tail.append(t)
+    return head, e_type, tail
+
+def load_ET(path, e2id, t2id, r2id):
+    head = []
+    e_type = []
+    tail = []
+    with open(path, encoding='utf-8') as r:
+        for line in r:
+            e, t = line.strip().split('\t')
+            e, t = e2id[e], t2id[t] + len(e2id)
+            head.append(e)
+            tail.append(t)
+            e_type.append(r2id['type'])
+    return head, e_type, tail
+
+
+def load_graph(data_dir, e2id, r2id, t2id, loadET=True, loadKG=True):
+    """
+    loading graph from dataset
+    
+    Parameters
+    ----------
+    data_dir: string
+    e2id: dict
+    r2id: dict
+    loadET, loadKG: boolean
+
+    Returns
+    -------
+    g: Graph
+    train_label: 2 dim tensor for representing relation between entity and entity type
+    all_true: 2 dim tensor
+    train_id: 1 dim tensor for representing entity id having entity type
+    valid_id: 1 dim tensor
+    test_id: 1 dim tensor
+    """
+    # load graph with input features, labels and edge type
+    train_label = load_labels([os.path.join(data_dir, 'ET_train.txt')], e2id, t2id)
+    train_id = train_label.sum(1).nonzero().squeeze()
+    valid_id = load_id(os.path.join(data_dir, 'ET_valid.txt'), e2id)
+    test_id = load_id(os.path.join(data_dir, 'ET_test.txt'), e2id)
+    all_true = load_labels([
+        os.path.join(data_dir, 'ET_train.txt'),
+        os.path.join(data_dir, 'ET_valid.txt'),
+        os.path.join(data_dir, 'ET_test.txt'),
+    ], e2id, t2id).half()
+    if loadKG:
+        head1, e_type1, tail1 = load_triple(os.path.join(data_dir, 'train.txt'), e2id, r2id)
+    else:
+        head1, e_type1, tail1 = [], [], []
+    if loadET:
+        head2, e_type2, tail2 = load_ET(os.path.join(data_dir, 'ET_train.txt'), e2id, t2id, r2id)
+    else:
+        head2, e_type2, tail2 = [], [], []
+
+    head = torch.LongTensor(head1 + head2 + tail1 + tail2)
+    tail = torch.LongTensor(tail1 + tail2 + head1 + head2)
+    g = dgl.graph((head, tail))
+
+    e_type1 = torch.LongTensor(e_type1)
+    e_type2 = torch.LongTensor(e_type2)
+    e_type = torch.cat([e_type1, e_type2, e_type1 + len(r2id), e_type2 + len(r2id)], dim=0)
+    g.edata['etype'] = e_type
+    if loadET:
+        g.ndata['id'] = torch.arange(len(e2id) + len(t2id))
+    else:
+        g.ndata['id'] = torch.arange(len(e2id))
+
+    return g, train_label, all_true, train_id, valid_id, test_id
