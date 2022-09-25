a1483b5adfebbe37a5127d60f2ab416eecc76519
Macho000 <saburotyan758@gmail.com>
2022-09-25 09:40:01 +0900

    20220925 add bert classification

--- eval.py
+++ eval.py
@@ -11,6 +11,7 @@ from JointGT import JointGT
 from T5 import T5
 from Bart import Bart
 from bert_score import score
+from bert_score import BERTScorer
 
 from transformers import BartTokenizer, T5Tokenizer
 from data import EntityTypingJointGTDataset, EntityTypingJointGTDataLoader, EntityTypingT5Dataset, EntityTypingT5DataLoader
@@ -28,7 +29,7 @@ def main(cfg: DictConfig) -> None:
     save_path = os.path.join(cfg.model.save_dir, cfg.data.name)
 
     # Initialize tokenizer
-    if cfg.model.name == 'JointGT': 
+    if cfg.model.name == 'JointGT':
         if cfg.model.pretrained_model == "Bart":
             tokenizer = BartTokenizer.from_pretrained(cfg.model.tokenizer_path)
         elif cfg.model.pretrained_model == "T5":
@@ -166,7 +167,12 @@ def main(cfg: DictConfig) -> None:
             predictions = f.readlines()
         with open(os.path.join(f'{cfg.model.test.file_path}_targets_test.txt'), 'r', encoding='utf-8') as f:
             targets = f.readlines()
+        logging.info("test")
         evaluation(cfg, targets, predictions)
+
+        prdcts, tgts = bert_classification(predictions, targets)
+        logging.info("test for classified token")
+        evaluation(cfg, tgts, prdcts)
 
 
 def evaluation(cfg, targets, predictions):
@@ -232,6 +238,154 @@ def calc_bert_score(targets, predictions):
     score_r = total_score_r/len(Recall)
     score_f1 = total_score_f1/len(F1)
     return score_p, score_r, score_f1
+
+
+def bert_classification(predictions_tokens, targets_tokens):
+    """
+    input:
+    predictions_tokens: 2dim list (num_predictions x num_separated_tokens)
+    targets_tokens: 2dim list (num_targets x num_separated_tokens)
+
+    output:
+    predicts: 1dim list
+    targets: 1dim list
+
+    e.g.
+    predictions_tokens
+    [['wikicat Living people',
+    'wikicat Hartlepool United F.C. players',
+    'wikicat Clyde F.C. players',
+    'wikicat Dundee F.C. players',
+    'wikicat St. Mirren F.C. players',
+    'wikicat Scottish Football League managers',
+    'wikicat Scotland international footballers',
+    'wikicat Forfar Athletic F.C. players',
+    'wikicat Dumbarton F.C.'],
+    ['wordnet person 100007846',
+    'wikicat Screenwriters',
+    'wikicat American people of English descent',
+    'wikicat People from Kansas City, Missouri',
+    'wikicat American film directors',
+    'wikicat Film producers',
+    'wikicat American screenwriters',
+    'wikicat American people',
+    'wikicat American film directors',
+    'wikicat American screenwriters',
+    'wikicat American people'],
+    ['wikicat Football Conference players',
+    'wikicat Living people',
+    'wikicat The Football League players',
+    'wikicat Hartlepool United F.C. players',
+    'wikicat Peterborough United F.C. players',
+    ...
+    ['wikicat Former countries in Europe',
+    'wikicat Former countries in the British Isles',
+    'wikicat Former countries in Europe'],
+    ['wikicat Provinces of Flanders',
+    'wordnet administrative district 108491826']]
+
+    targets_tokens
+    [['wikicat St. Mirren F.C. players'],
+    ['wikicat Film directors from Missouri'],
+    ['wikicat Notts County F.C. players'],
+    ['wikicat States and territories established in 1649'],
+    ['wikicat Provinces of Belgium']]
+
+
+    output:
+    predicts
+    ['wikicat St. Mirren F.C. players', 'wikicat People from Kansas City, Missouri', 'wikicat Notts County F.C. players', 
+    'wikicat Former countries in the British Isles', 'wikicat Provinces of Flanders']
+
+    targets:
+    ['wikicat St. Mirren F.C. players', 'wikicat Film directors from Missouri', 'wikicat Notts County F.C. players',
+    'wikicat States and territories established in 1649', 'wikicat Provinces of Belgium']
+    """
+    list_all_combination_target = []
+    list_all_combination_prediction = []
+    list_combination_count = []
+    for i in range(len(targets_tokens)):
+        for j in range(len(targets_tokens[i])):
+            for k in range(len(predictions_tokens[i])):
+                list_all_combination_target.append(targets_tokens[i][j])
+                list_all_combination_prediction.append(predictions_tokens[i][k])
+        list_combination_count.append(len(targets_tokens[i]) * len(predictions_tokens[i]))
+
+    scorer = BERTScorer(lang="en")
+    Precision, Recall, F1 = scorer.score(list_all_combination_prediction, list_all_combination_target)
+
+    def cumsum(comb_list: list):
+        sum_comb_list = []
+        sum = 0
+        for item in comb_list:
+            sum_comb_list.append(sum+item)
+            sum += item
+        return sum_comb_list
+    list_combination_count_with_zero = [0] + list_combination_count
+    sum_combination_count = cumsum(list_combination_count_with_zero)
+        
+    list_comb = []
+    for i in range(1, len(list_combination_count_with_zero)):
+        tmp_combination_targets = list_all_combination_target[sum_combination_count[i-1]:sum_combination_count[i]]
+        tmp_combination_predictions = list_all_combination_prediction[sum_combination_count[i-1]:sum_combination_count[i]]
+        tmp_F1 = F1[sum_combination_count[i-1]:sum_combination_count[i]]
+        assert len(tmp_combination_targets) == len(tmp_combination_predictions)
+        assert len(tmp_combination_predictions) == len(tmp_F1)
+
+        # loop until tmp_F1 is null
+        while len(tmp_F1) != 0:
+            # get max index
+            max_index = torch.argmax(tmp_F1)
+
+            # get combination
+            list_comb.append(tuple((tmp_combination_predictions[max_index], tmp_combination_targets[max_index])))
+
+            # remove entity from tmp_combination_targets and tmp_combination_predictions
+            list_delete_target_index = []
+            list_delete_prediction_index = []
+            for index, item in enumerate(tmp_combination_targets):
+                if item == tmp_combination_targets[max_index]:
+                    list_delete_target_index.append(index)
+            for index, item in enumerate(tmp_combination_predictions):
+                if item == tmp_combination_predictions[max_index]:
+                    list_delete_prediction_index.append(index)
+            list_select_index = []
+            for i in range(len(tmp_F1)):
+                if (
+                        len(list_delete_prediction_index) > 0
+                        and i == list_delete_prediction_index[0]
+                        and len(list_delete_target_index) > 0
+                        and i == list_delete_target_index[0]
+                   ):
+                    list_delete_prediction_index.pop(0)
+                    list_delete_target_index.pop(0)
+                elif len(list_delete_prediction_index) > 0 and i == list_delete_prediction_index[0]:
+                    list_delete_prediction_index.pop(0)
+                elif len(list_delete_target_index) > 0 and i == list_delete_target_index[0]:
+                    list_delete_target_index.pop(0)
+                else:
+                    list_select_index.append(i)
+            if len(list_select_index) > 0:
+                tmp_F1 = torch.index_select(tmp_F1, 0, torch.tensor(list_select_index))
+                tmp_combination_targets = [item for i, item in enumerate(tmp_combination_targets) if i in list_select_index]
+                tmp_combination_predictions = [item for i, item in enumerate(tmp_combination_predictions) if i in list_select_index]
+            else:
+                tmp_F1 = torch.Tensor()
+                tmp_combination_targets = []
+                tmp_combination_predictions = []
+            assert len(tmp_combination_targets) == len(tmp_combination_predictions)
+            assert len(tmp_combination_predictions) == len(tmp_F1)
+
+    def get_target_predict(list_comb):
+        predicts = []
+        targets = []
+        for index_list_comb in range(len(list_comb)):
+            predict, target = list_comb[index_list_comb]
+            predicts.append(predict)
+            targets.append(target)
+        return predicts, targets
+    predicts, targets = get_target_predict(list_comb)
+    return predicts, targets
 
 
 if __name__ == '__main__':
